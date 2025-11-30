2514c1631e3368bc9d6758d9c939e4c438d94de3
Kyle Tse <shtse8@gmail.com>
2025-11-30 23:44:04 +0000

    fix: externalize solid-js and @solidjs/router in theme-default build

--- packages/theme-default/bunup.config.ts
+++ packages/theme-default/bunup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "bunup";
 export default defineConfig({
 	target: "browser",
 	platform: "browser",
+	external: ["solid-js", "solid-js/*", "@solidjs/router"],
 	jsx: {
 		runtime: "automatic",
 		importSource: "solid-js",
